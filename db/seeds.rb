b6cbe8028fe22b486419a5918590213d2f5e5381
jizampa <jizampa@gmail.com>
2018-10-03 03:25:16 +0000

    after runnig the seed and populating the Player table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+i = 0
+while i < 100
+    new_player = Player.create( :name => Faker::FunnyName.name,
+                   :age => Faker::Number.between(10, 70),
+                   :email => Faker::SiliconValley.email,
+                   :phone => Faker::PhoneNumber.cell_phone)
+    i += 1
+    end

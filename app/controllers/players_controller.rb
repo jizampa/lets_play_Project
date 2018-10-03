01c7023370da93226af660231a51a661d3dc50d3
jizampa <jizampa@gmail.com>
2018-10-03 11:42:19 +0000

    /players working, retrieving all players and showing on the view

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,6 @@
 class PlayersController < ApplicationController
   def index
+
+    @players = Player.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
- 
-  get 'players/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+ get '/players', to: 'players#index'
 end

69a4d882260c3f7a7fa3bca30403908e91506ab4
jizampa <jizampa@gmail.com>
2018-10-07 20:39:09 -0500

    Pagination working

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,8 +1,8 @@
 class PlayersController < ApplicationController
   def index
  
-        @players = Player.all
+        @players = Player.all.paginate(page: params[:page], per_page: 7)
         @parks = Park.all
-        @arena = Arena.all
+        @arena = Arena.all.paginate(page: params[:page], per_page: 8)
   end
 end

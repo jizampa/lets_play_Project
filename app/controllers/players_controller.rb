52d3520d083ee3229b7785556644c339d3170fc8
jizampa <jizampa@gmail.com>
2018-10-08 12:26:24 -0500

    search partially working

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,5 +4,18 @@ class PlayersController < ApplicationController
         @players = Player.all.paginate(page: params[:page], per_page: 5)
         @parks = Park.all
         @arena = Arena.all.paginate(page: params[:page], per_page: 5)
+
+        @players_searched = Player.where("name LIKE ?", "%#{params[:q]}%")
+       
+
+      
+        
+
+        
   end
+
+  def show
+    @players_searched = Player.where("name = ?", params[:name])
+  end
+
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,5 @@
 class Player < ApplicationRecord
-    has_many :player_battle_arena
+    has_many :arena
     has_many :parks, through: :arena
     validates :name, :age, :email, presence: true
 

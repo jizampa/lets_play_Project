14a1ec207dd0ae92faf291269ccd67818d65e83e
jizampa <jizampa@gmail.com>
2018-10-11 15:22:44 -0500

    link to the player working

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'about_page/index'
- get '/letsplay', to: 'letsplay#index', as: 'home'
- get '/letsplay/players', to: 'players#index', as: 'players'
- get '/letsplay/players/:name', to: 'players#index'
 
- get '/letsplay/about', to: 'about_page#index', as: 'about'
+#  get '/letsplay', to: 'letsplay#index', as: 'home'
+#  get '/letsplay/players', to: 'players#index', as: 'players'
 
+#  get '/letsplay/players/:name', to: 'players#index'
+get '/about', to: 'about_page#index'
+resources :players, only: [:index, :show]
+ 
+root to: "letsplay#index"
 end

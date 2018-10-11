0cf1a7609039b82291ce5af823520026b412e867
jizampa <jizampa@gmail.com>
2018-10-11 14:52:40 -0500

    Nav working

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -6,21 +6,12 @@ class PlayersController < ApplicationController
         @arena = Arena.all.paginate(page: params[:page], per_page: 5)
 
         @players_searched = Player.where("name LIKE ?", "%#{params[:q]}%").paginate(page: params[:page], per_page: 5)
-
-        @player_Arena = Player.find(3301).arena
-     
-
-
-       
-
-      
-        
-
         
   end
 
   def show
     @players_searched = Player.where("name = ?", params[:name])
   end
+  
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'about_page/index'
- get '/letsplay', to: 'letsplay#index'
+ get '/letsplay', to: 'letsplay#index', as: 'home'
  get '/letsplay/players', to: 'players#index', as: 'players'
  get '/letsplay/players/:name', to: 'players#index'
 
- get '/letsplay/about', to: 'about_page#index', as: 'About'
+ get '/letsplay/about', to: 'about_page#index', as: 'about'
 
 end

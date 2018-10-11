b89ec3837db58e8990590cc53a0e1f32bef97315
jizampa <jizampa@gmail.com>
2018-10-11 16:25:11 -0500

    Cleaning code to submmit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
 
-#  get '/letsplay', to: 'letsplay#index', as: 'home'
-#  get '/letsplay/players', to: 'players#index', as: 'players'
-
-#  get '/letsplay/players/:name', to: 'players#index'
 get '/about', to: 'about_page#index'
 resources :players, only: [:index, :show]
  

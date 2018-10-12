0c7d4c4227324382a6b9cce53046458a7f57c5bb
jizampa <jizampa@gmail.com>
2018-10-11 22:11:38 -0500

    DONE

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -5,7 +5,7 @@ class PlayersController < ApplicationController
         @parks = Park.all
         @arena = Arena.all.paginate(page: params[:page], per_page: 5)
 
-        @players_searched = Player.where("name LIKE ?", "%#{params[:q]}%").paginate(page: params[:page], per_page: 12)
+        @players_searched = Player.where("name LIKE ?", "%#{params[:q]}%").paginate(page: params[:page], per_page: 9)
         
   end
 
